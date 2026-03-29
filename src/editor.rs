5a2e790f4a44e57cf9af7eca1483503b7425c0b3
DoomSlayer-6969 <raishreyash52@gmail.com>
2026-03-29 17:16:04 +0530

    fix: Improve drawing

--- src/editor.rs
+++ src/editor.rs
@@ -1,6 +1,7 @@
 use crossterm::event::{Event, Event::Key, KeyCode::Char, KeyEvent, KeyModifiers, read};
+use std::io::Error;
 mod terminal;
-use terminal::Terminal;
+use terminal::{Position, Size, Terminal};
 
 pub struct Editor {
     should_quit: bool,
@@ -18,6 +19,18 @@ impl Editor {
         result.unwrap();
     }
 
+    fn repl(&mut self) -> Result<(), Error> {
+        loop {
+            self.refresh_screen()?;
+            if self.should_quit {
+                break;
+            }
+            let event = read()?;
+            self.evaluate_event(&event);
+        }
+        Ok(())
+    }
+
     fn evaluate_event(&mut self, event: &Event) {
         if let Key(KeyEvent {
             code, modifiers, ..
@@ -32,36 +45,28 @@ impl Editor {
         }
     }
 
-    fn refresh_screen(&self) -> Result<(), std::io::Error> {
+    fn refresh_screen(&self) -> Result<(), Error> {
+        Terminal::hide_cursor()?;
         if self.should_quit {
             Terminal::clear_screen()?;
-            print!("Goodbye.\r\n");
+            Terminal::print("Goodbye.\r\n")?;
         } else {
             Self::draw_rows()?;
-            Terminal::move_cursor_to(0, 0)?;
+            Terminal::move_cursor_to(Position { x: 0, y: 0 })?;
         }
+        Terminal::show_cursor()?;
+        Terminal::execute()?;
         Ok(())
     }
 
-    fn draw_rows() -> Result<(), std::io::Error> {
-        let height = Terminal::size()?.1;
+    fn draw_rows() -> Result<(), Error> {
+        let Size { height, .. } = Terminal::size()?;
         for current_row in 0..height {
-            print!("~");
+            Terminal::clear_line()?;
+            Terminal::print("~")?;
             if current_row + 1 < height {
-                print!("\r\n");
+                Terminal::print("\r\n")?;
             }
-        }
-        Ok(())
-    }
-
-    fn repl(&mut self) -> Result<(), std::io::Error> {
-        loop {
-            self.refresh_screen()?;
-            if self.should_quit {
-                break;
-            }
-            let event = read()?;
-            self.evaluate_event(&event);
         }
         Ok(())
     }
